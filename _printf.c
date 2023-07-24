e9d40f16e95bfdbe65504b2e283cc6189b34c7bc
Chibuike Favour <iamkorra03@gmail.com>
2023-07-24 15:24:37 +0100

    it failed so i returend it

--- _printf.c
+++ _printf.c
@@ -1,64 +1,50 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-
 /**
- * _printf - A function to print char, string, and handle %
- * @format: A variable that is used to change the condition of the file
+ * _printf - A function to print char, string and hanle %
+ * @format: A varible that is used to chnage the condition of the file
  * @...: Variable list of arguments
  * Return: Always count of the argument
  */
 int _printf(const char *format, ...)
 {
-    int count = 0, i;
-    va_list args;
+	int count = 0, i;
+	va_list args;
 
-    functionstruct arrayStructFunction[6] = {
-        {'c', _putchar}, {'s', _putstring}, {'d', _printint},
-        {'i', _printint}, {'u', _printint}, {'%', _printpercent}};
-    
-    va_start(args, format);
-    if (format == NULL)
-        return (-1);
-    
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '%')
-            {
-                write(1, "%", 1);
-                count++, format++;
-            }
-            else if (*format == ' ' || *format == '\0')
-            {
-                return (-1);
-            }
-            else if (*(format + 1) == '\0')
-            {
-                return (-1);
-            }
-            else
-            {
-                for (i = 0; i < 6; i++)
-                {
-                    if (arrayStructFunction[i].c == *format)
-                    {
-                        count += arrayStructFunction[i].fpointer(args, (char *)format);
-                        break;
-                    }
-                }
-                format++;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            count++, format++;
-        }
-    }
-    
-    va_end(args);
-    return (count);
+	functionstruct arrayStructFunction[5] = {
+		{'c', _putchar}, {'s', _putstring}, {'d', _printint},
+		{'i', _printint}, {'u', _printint}};
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+				write(1, "%", 1), count++;
+			else if (*format == ' ' || *format == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				for (i = 0; i < 5; i++)
+				{
+					if (arrayStructFunction[i].c == *format)
+						count += arrayStructFunction[i].fpointer(args, (char *)format);
+				}
+			}
+			format++;
+		}
+		else
+		{
+			write(1, format, 1);
+			count++, format++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
