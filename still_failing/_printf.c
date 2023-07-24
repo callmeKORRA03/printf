628a682c5fd3a1ed78ce6d791224e3a8186d7296
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-07-24 08:47:51 +0100

    Reverted changes to previous file

--- still_failing/_printf.c
+++ still_failing/_printf.c
@@ -12,38 +12,32 @@ int _printf(const char *format, ...)
 	int count = 0, i;
 	va_list args;
 
-	functionstruct arrayStructFunction[5] = {
-		{'c', _putchar}, {'s', _putstring}, {'d', _printint}, {'i', _printint},
-		{'u', _printint}};
+	functionstruct arrayStructFunction[6] = {
+		{'c', _putchar}, {'s', _putstring}, {'d', _printint},
+		{'i', _printint}, {'u', _printint}, {'%', _printpercent}};
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
+flag1:
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '%')
-				write(1, "%", 1), count++;
-			else if (*format == ' ' || *format == '\0')
+			while (*format == ' ')
 			{
-				return (-1);
+				if (*format == '\0' || *(format + 1) == '\0')
+					return (-1);
+				format++;
 			}
-			else
-			{
-				for (i = 0; i < 5; i++)
-				{
-					if (arrayStructFunction[i].c == *format)
-						count += arrayStructFunction[i].fpointer(args, (char *)format);
-				}
-			}
-			format++;
+			for (i = 0; i < 6; i++)
+				if (arrayStructFunction[i].c == *format)
+					count += arrayStructFunction[i].fpointer(args, (char *)format);
+			format++, format++;
+			goto flag1;
 		}
-		else
-		{
-			write(1, format, 1);
-			count++, format++;
-		}
+		write(1, format, 1);
+		count++, format++;
 	}
 	va_end(args);
 	return (count);
