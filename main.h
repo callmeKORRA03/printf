ce1778e5cf971a14f114534886f190a6f440609b
Chibuike Favour <iamkorra03@gmail.com>
2023-07-25 15:30:07 +0100

    finally fixed the error

--- main.h
+++ main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /*Include standard header files*/
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-
 /*Struct Definition for Function Pointers*/
-
 /**
  * functionstruct - A struct that holds a character key
  * mapped to a function pointer. Simulating key-value pair
@@ -15,13 +12,11 @@
  * @fpointer: Pointer to specific function that receives
  * a string and an arguments list
  */
-
 typedef struct functionstruct
 {
 	const char c;
 	int (*fpointer)(va_list, char *);
 } functionstruct;
-
 /* Helper Function prototypes */
 int _putchar(va_list, char *);
 int _putstring(va_list, char *);
@@ -29,8 +24,9 @@ int _printint(va_list, char *);
 int _printpercent(va_list, char *);
 int _printbinary(va_list, char *str);
 void handleformatprint1(int *, char *, va_list);
+void convert_negative(unsigned int *positiveNum, int retrieved_number);
+void print_buffer_int(char buff[11], int index, int *counter);
 
 /*Main _printf Function Prototype*/
 int _printf(const char *format, ...);
-
 #endif /* MAIN_H */
