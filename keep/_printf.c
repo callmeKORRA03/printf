e6ee867ba3d455c128f64fc502ff44e303747309
Chibuike Favour <iamkorra03@gmail.com>
2023-07-25 15:14:13 +0100

    Revert "Refactored Print Integer function to handle extra test cases - Testing checker now"
    This reverts commit 1a423a8b8fac67dc7d698e3b501b1ac6b478ee08.

---
+++ keep/_printf.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - A function to print char, string and hanle %
+ * @format: A varible that is used to chnage the condition of the file
+ * @...: Variable list of arguments
+ * Return: Always count of the argument
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0, i;
+	va_list args;
+
+	functionstruct arrayStructFunction[7] = {
+		{'c', _putchar}, {'s', _putstring}, {'d', _printint}, {'i', _printint},
+		{'u', _printint}, {'b', _printbinary}, {'%', _printpercent}};
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if ((*format == ' ' && *(format + 1) == '\0') || *format == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				for (i = 0; i < 7; i++)
+				{
+					if (arrayStructFunction[i].c == *format)
+						count += arrayStructFunction[i].fpointer(args, (char *)format);
+				}
+			}
+			format++;
+		}
+		else
+		{
+			write(1, format, 1);
+			count++, format++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
