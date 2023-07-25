ae8267ddd76193e644a7d6f8457cc23b33327d9c
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-07-25 16:43:40 +0100

    House Cleaning the directories used during testing

--- Housekeeping/old_files/dummy-testing/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
-/*Function prototypes */
-int _putchar(char c);
-int _putstring(char *str);
-int _printf(const char *format, ...);
-int _printint(int num);
-#endif /* MAIN_H */
